e98352c7a1658ec1b5e1b8705b2e200f2479ac7f
camila paiva <camila.acacio.paiva@gmail.com>
2018-02-24 15:06:45 -0300

    Update Maven artifact version example.
    test15

--- src/main/java/com/clearspring/analytics/stream/cardinality/HyperBitBit.java
+++ src/main/java/com/clearspring/analytics/stream/cardinality/HyperBitBit.java
@@ -28,10 +28,11 @@
   * <p/>
   */
  
-package com.clearspring.analytics.stream.cardinality;
+package com.clearspring.experimental.stream.cardinality;
 
-import com.clearspring.analytics.hash.MurmurHash;
 
+import com.clearspring.analytics.stream.cardinality.ICardinality;
+import com.clearspring.analytics.stream.cardinality.CardinalityMergeException;
 import java.io.IOException;
 
 public class HyperBitBit implements ICardinality {
